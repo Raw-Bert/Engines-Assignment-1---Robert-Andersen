005ff4b6c699be0cc5b641bf191b663c1e296ac2
Rob <robert.andersen@ontariotechu.net>
2019-09-25 15:08:44 -0400

    Player movement and camera movement

---
+++ Engines Induvidual Assignemt 1 - 100658473/Assets/_Scripts/MovementTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour
+{
+
+    public float speed;
+    Vector3 rot = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += dir * speed;
+    }
+}
