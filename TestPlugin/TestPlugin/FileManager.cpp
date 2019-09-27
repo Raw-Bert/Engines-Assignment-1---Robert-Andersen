bd5019edcf2cfe29d9b7311886976232257c0b27
Rob <robert.andersen@ontariotechu.net>
2019-09-26 23:08:03 -0400

    Removed files cause they don't work and I'm not going to be around tomorrow so I want to hand in what I can and see if i can find time to finish tomorrow

---
+++ TestPlugin/TestPlugin/FileManager.cpp
@@ -0,0 +1,11 @@
+#include "FileManager.h"
+#include <iostream>
+#include <fstream>
+
+void saveToFile()
+{
+	ofstream coordFile;
+	coordFile.open("CoordFile.txt");
+	coordFile << //put coords here and format them
+		coordFile.close();
+}
